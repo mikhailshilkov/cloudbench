1cafcddc734f3047e9a37a6b412087ad2c14d111
Mikhail Shilkov <mikhail.shilkov@prodrive-technologies.com>
2018-09-14 21:08:03 +0200

    Scheduler WIP

---
+++ scheduler/index.ts
@@ -0,0 +1,23 @@
+import { functionApp } from "@pulumi/azure-serverless";
+
+// Create an Azure function that prints a message and the request headers.
+function handler(context: functionApp.Context, request: functionApp.HttpRequest) {
+    let body = "";
+    let headers = context.req!.headers!;
+    for (let h in headers) {
+        body = body + `${h} = ${headers[h]}\n`;
+    }
+
+    let res: functionApp.HttpResponse = {
+        status: functionApp.HttpStatusCode.OK,
+        headers: {
+            "content-type": "text/plain",
+        },
+        body: `Greetings from Azure Functions!\n\n===\n\n${body}`,
+    };
+
+    context.done(undefined, res);
+}
+
+let fn = new functionApp.HttpFunction("fn", handler);
+export let endpoint = fn.endpoint;
